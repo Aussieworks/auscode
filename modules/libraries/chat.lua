0c56d06fc8a16d5e1f3d6d1dedce55af9f808bcc
ChickenMst <mgeek602@gmail.com>
2026-03-27 14:41:51 +1100

    add `announceGroup` to modules

--- modules/libraries/chat.lua
+++ modules/libraries/chat.lua
@@ -10,3 +10,17 @@ function modules.libraries.chat:announce(title, message, target)
     server.announce(title, message, target) -- send the message to the server
     table.insert(self.messages, {title = title, message = message, target = target}) -- add the message to the messages table
 end
+
+---@param title string
+---@param message string
+---@param ... number|table the target player IDs to send the announcement to
+function modules.libraries.chat:announceGroup(title, message, ...) -- send an announcement to a group of players
+    local targets = table.pack(...)
+    if type(targets[1]) == "table" then -- if a table has been passed in set targets to that table
+        targets = targets[1]
+    end
+    for _, target in pairs(targets) do
+        server.announce(title, message, target) -- announce to each individual player
+    end
+    table.insert(self.messages, {title = title, message = message, target = targets}) -- add the message to the messages table
+end
